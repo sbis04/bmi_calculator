5b2ef077bdf77f4198ec7dc4a0a2671ee0998d0d
Souvik Biswas <sbis1999@gmail.com>
2020-02-10 19:45:39 +0530

    Delete flutter_export_environment.sh

--- bmi_flutter/.ios/Flutter/flutter_export_environment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/souvikbiswas/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/souvikbiswas/Mobile Apps/Flutter/bmi_calculator/bmi_flutter"
-export "FLUTTER_TARGET=lib/main.dart"
-export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
